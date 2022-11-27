fd0612b1084f05062ee47b30c7c48617d10f7d65
hoojianliang <hoojianliang@users.noreply.github.com>
2022-11-27 23:58:06 +0800

    implement ui skeleton

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <button
-      class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
+  <div class="container mx-auto mt-8">
+    <div class="grid grid-cols-3 gap-4">
+      <ApartmentList />
+      <InventoryList />
+    </div>
   </div>
 </template>
---
+++ components/ApartmentList.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul class="menu bg-base-100 shadow-xl">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+        <li><a>Item 3</a></li>
+    </ul>
+</template>
